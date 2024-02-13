8f5b6bb0da5cec36b5ee1d4066418f28583be59d
Viswanathan <104837760+viswa-gz@users.noreply.github.com>
2024-02-13 11:03:05 +0530

    update sql query

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php 
 
 /**
- * Uninstall the table when user delete the Gozen forms plugin.
+ * Uninstall the table when user delete the Gozen Engage plugin.
  */
 
 function deleteGzEngageTable($tablename) {
@@ -13,12 +13,8 @@ function deleteGzEngageTable($tablename) {
     //Global variable
     global $wpdb;
 
-    $table_name = $wpdb->prefix.$tablename;
+    $wpdb->query( $wpdb->prepare("DROP TABLE IF EXISTS %s","$wpdb->prefix.$tablename") );
 
-    // delete the table.
-    $sql = "DROP TABLE IF EXISTS $table_name";
-
-    $wpdb->query($sql);
     delete_option("my_plugin_db_version"); 
 
 }
