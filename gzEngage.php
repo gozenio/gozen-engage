4514ab7eba09338320427c7ca1791b171f4e021b
Viswanathan <104837760+viswa-gz@users.noreply.github.com>
2024-02-15 11:05:50 +0530

    update license prop in main file header.

--- gzEngage.php
+++ gzEngage.php
@@ -6,9 +6,12 @@
  * Description: Generate Leads With AI-Powered Interactive Content And Gamification
  * Author:GozenHQ
  * Version:1.0.0
+ * Requires at least: 4.6.1
+ * Requires PHP:      7.2
  * Text Domain:gozenengage
  * Domain Path: /languages/
- * Requires at least: 4.6.1
+ * License: GPL v2 or later
+ * License URI:https://www.gnu.org/licenses/gpl-2.0.html
  * Contributors:GozenHq
  */
 
